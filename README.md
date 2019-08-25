88fda9d8209307cd82f870d83c65357259c271f1
Paul Kent <paul.kent@sas.com>
2019-08-25 15:40:06 -0400

    attended to LICENSE details  credit to split() and diff() code from other projects  contributions section added to README

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# ky
-ky - kubernetes yaml swiss army knife
 
-ky diff file1 file2 -- compare two yamls
-ky split big-ole.yaml -- split bunches of yaml into name-type.yaml or name/type.yaml
+# ky - a kubernetes yaml swiss army knife
 
+compare two yamls with `ky diff file1 file2`
+
+split a monster yaml into parts with `ky split big-ole.yaml`
+
+# credits
+
+The diff subroutines came from [Sahil Muthoo](https://github.com/sahilm/yamldiff)
+
+The split subroutines came from [Frederik Mogensen](https://github.com/mogensen/kubernetes-split-yaml)
+
+Their programming style was harmonized somewhat to mine, and the combined works published as KY, which operates in the kubectl style of {command} {verb} {noun}
+
+# contributing
+
+The KY project welcomes contributions.  
+
+* fork the repo
+* clone/pull from the fork
+* create a branch for the improvement
+* commit to the fork
+* raise a merge-request to splice your improvement into the KY project
+* "cheers!"
+
+There is a nice discussion of this [contribution workflow](https://github.com/freeCodeCamp/how-to-contribute-to-open-source/blob/master/CONTRIBUTING.md) on github.
--- cmd/split.go
+++ cmd/split.go
@@ -1,10 +1,16 @@
 package cmd
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"io"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 // splitCmd represents the split command
@@ -28,8 +34,88 @@ func init() {
 	rootCmd.AddCommand(splitCmd)
 }
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const outdir = "generated/"
+
+// KubernetesAPI is for unmarshaling objects
+type KubernetesAPI struct {
+	APIVersion string `yaml:"apiVersion"`
+	Kind       string `yaml:"kind"`
+	Metadata   struct {
+		Name   string `yaml:"name"`
+		Labels struct {
+			Source string `yaml:"source"`
+		} `yaml:"labels"`
+	} `yaml:"metadata"`
+}
+
 // Split takes
 func Split(args []string) error {
+
 	log.Println("Hello from Split", args[0])
+
+	files := readAndSplitFile(args[0])
+
+	os.Mkdir(outdir, os.ModePerm)
+
+	for key, value := range files {
+
+		var m KubernetesAPI
+
+		err := yaml.Unmarshal([]byte(value), &m)
+		check(err)
+
+		if m.Kind == "" {
+			fmt.Println("yaml file with no kind")
+		} else {
+
+			fmt.Println("File: ", key, m.Kind)
+
+			filename := outdir + m.Metadata.Name + "-" + m.Kind + ".yaml"
+			fmt.Println(filename)
+
+			f, err := os.Create(filename)
+			check(err)
+			defer f.Close()
+
+			_, err = f.WriteString(value)
+			check(err)
+		}
+	}
+
 	return nil
+}
+
+func readAndSplitFile(logfile string) map[int]string {
+
+	f, err := os.OpenFile(logfile, os.O_RDONLY, os.ModePerm)
+	check(err)
+
+	defer f.Close()
+
+	rd := bufio.NewReader(f)
+	c := make(map[int]string)
+	count := 0
+
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Panicf("read file line error: %v", err)
+		}
+		prettyline := strings.TrimSpace(line)
+		if prettyline == "---" {
+			count++
+		} else {
+			c[count] += line
+		}
+	}
+	return c
 }
